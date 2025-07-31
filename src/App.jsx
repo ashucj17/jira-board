875f897f022abc01124632918e81bc8e966f43e5
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-07-31 23:57:34 +0530

    Header Structure & Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import TaskQueue from "./Components/Tasks/tasks-queue";
-import './App.css'
+import "./App.css";
 
-const App = () =>{
-  return(
+const App = () => {
+  return (
     <div className="app">
-    <header className="app__header">
-      <h1 style={{textAlign:"center" , margin: '50px auto'}}>Jira Board</h1>
-      <TaskQueue />
-    </header>
-    <div className="app__section">
-      <section className="section">section1</section>
-      <section className="section">section2</section>
-      <section className="section">section3</section>
-      <section className="section">section4</section>
+      {/* <header className="app__header"></header> */}
+        <h1 style={{ textAlign: "center", margin: "50px auto" }}>Jira Board</h1>
+        <TaskQueue />
+      
+      <div className="app__section">
+        <section className="section">section1</section>
+        <section className="section">section2</section>
+        <section className="section">section3</section>
+        <section className="section">section4</section>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Components/Tasks/tasks-queue.jsx
+++ src/Components/Tasks/tasks-queue.jsx
@@ -1,25 +1,33 @@
-const TaskQueue = ()=>{
-    return(
-        <header className="app__header">
-        <input type='text' placeholder='Enter Task Detail' />
-      <div className='queue'>
-      <div className='queue__assigned'>
-        <button className='queue__assigned-role'>Dev</button>
-        <button className='queue__assigned-role'>QA</button>
-        <button className='queue__assigned-role'>Product Owner</button>
-        </div>
-        <div className='queue__state'>
-            <select>
-                <option value="Ready for development">Ready for development</option>
-                <option value="In Progress">In Progress</option>
-                <option value="Ready for QA">Ready for QA</option>
-                <option value="Closed">Closed</option>
-            </select>
-            <button type='submit' value='submit'>+ Add</button>
-        </div>
-      </div>
-    </header>
-    )
-}
+import "./tasks-queue.css";
 
-export default TaskQueue
+const TaskQueue = () => {
+  return (
+    <header className="app__header">
+      <form>
+        <input type="text" placeholder="Enter Task Detail" />
+        <div className="queue">
+          <div className="queue__assigned">
+            <button className="queue__assigned-role">Dev</button>
+            <button className="queue__assigned-role">QA</button>
+            <button className="queue__assigned-role">Product Owner</button>
+          </div>
+          <div className="queue__state">
+            <select>
+              <option value="Ready for development">
+                Ready for development
+              </option>
+              <option value="In Progress">In Progress</option>
+              <option value="Ready for QA">Ready for QA</option>
+              <option value="Closed">Closed</option>
+            </select>
+            <button type="submit" value="submit">
+              + Add
+            </button>
+          </div>
+        </div>
+      </form>
+    </header>
+  );
+};
+
+export default TaskQueue;
