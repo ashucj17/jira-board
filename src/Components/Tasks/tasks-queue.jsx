5af33376f7b7e7756b944267c941dd0b36b9b39e
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-08-06 21:59:46 +0530

    Handling multiple states using single state variable

--- src/Components/Tasks/tasks-queue.jsx
+++ src/Components/Tasks/tasks-queue.jsx
@@ -3,23 +3,44 @@ import Tags from "../Tags/queue-assigned-tags";
 import "./tasks-queue.css";
 
 const TaskQueue = (props) => {
-  const [input, setInput] = useState("")
-  const [value, setValue] = useState('');
+  const [taskData, setTaskData] = useState(
+    {
+      task: '',
+      status: ''
+    }
+  )
+  console.log(taskData);
 
-  const inputHandler= (e)=>{
-  setInput(e.target.value)
-  }
-  console.log(input)
+  const handleChange = (e) =>{
+    const {name, value} = e.target
+    setTaskData((prev)=>{
+      return(
+         {...prev,[name]:value}
+      )
+    })
 
-  const selectTask= (e) => {
-    setValue(e.target.value)
   }
-  console.log(value)
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    console.log(taskData)
+  }
+  // const [input, setInput] = useState("")
+  // const [value, setValue] = useState('');
+
+  // const inputHandler= (e)=>{
+  // setInput(e.target.value)
+  // }
+  // console.log(input)
+
+  // const selectTask= (e) => {
+  //   setValue(e.target.value)
+  // }
+  // console.log(value)
 
   return (
     <header className="app__header">
-      <form>
-        <input type="text" placeholder="Enter Task Detail" onChange={inputHandler} />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="task" placeholder="Enter Task Detail" onChange={handleChange} />
         <div className="queue">
           <div className="queue__assigned">
            <Tags tagName={"Dev"} />
@@ -27,7 +48,7 @@ const TaskQueue = (props) => {
            <Tags tagName={"Product Owner"} />
           </div>
           <div className="queue__state">
-            <select onChange={selectTask}>
+            <select onChange={handleChange} name="status">
               <option value="Ready for development">
                 Ready for development
               </option>
