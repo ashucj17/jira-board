e31ed97368d4ccc7bd45e10b9f51c5040e6e469a
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-08-19 12:54:58 +0530

    removed branch conflict

--- src/Components/TaskSection/task-section-column.jsx
+++ src/Components/TaskSection/task-section-column.jsx
@@ -11,11 +11,11 @@ const TaskSection= ({title, tasks, status, icon, handleDelete, setActiveCard, on
         
             <h2 className='task__section_column'> <img src={icon} alt='' className='task__section_column-icon' /> {title}
             </h2>
-<<<<<<< HEAD
+
             <DropArea  onDrop={()=>{onDrop(status, 0)}} />
-=======
+
             <DropArea  />
->>>>>>> 8b512378dd474c60d2de1324dcb02d5f9b2ab9fb
+
             {tasks.map((task, index) => task.status === status &&  (
                 <React.Fragment key={index}>
                 <TaskCard key={index} icon={BinCard} title={task.task} tags={task.tags} handleDelete={handleDelete} index={index} setActiveCard={setActiveCard} />
